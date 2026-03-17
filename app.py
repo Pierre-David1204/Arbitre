466db98bacbf8de3e4e72d034188f1f82915a1b9
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-17 10:56:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.title("⚖️ Application arbitre")
+
+terrains = [1,2,3,4,5,6]
+
+terrain = st.selectbox(
+    "Choisir un terrain",
+    terrains
+)
+
+if st.button("Valider"):
+
+    st.session_state.terrain = terrain
+
+    st.switch_page("pages/1_Matchs.py")
